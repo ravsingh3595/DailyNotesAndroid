9b02673c5bc8c344b640f1d103ec460c67ae01ee
user <ravneets47@gmail.com>
2018-12-09 13:21:46 -0500

    Database file modified

--- app/src/main/java/com/androidproject/dailynotesandroid/Database/DBSubject.java
+++ app/src/main/java/com/androidproject/dailynotesandroid/Database/DBSubject.java
@@ -2,9 +2,13 @@ package com.androidproject.dailynotesandroid.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.androidproject.dailynotesandroid.Model.Subject;
+
+import java.util.ArrayList;
 
 public class DBSubject {
 
@@ -33,6 +37,55 @@ public class DBSubject {
         database.insert(TABLE_SUBJECT, null, contentValues);
         database.close();
 
+    }
+
+    public void updateSubject(Subject subject)
+    {
+        dbHelper = new DBHelper(context);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(SUBJECT_NAME, subject.getSubjectName());
+
+        database.update(TABLE_SUBJECT, contentValues, SUBJECT_ID + "=?", new String[]{String.valueOf(subject.getSubjectId())});
+        database.close();
+
+    }
+
+    public void deleteSubject(Subject subject)
+    {
+        dbHelper = new DBHelper(context);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        database.delete(TABLE_SUBJECT, SUBJECT_ID + "=?", new String[]{String.valueOf(subject.getSubjectId())});
+        database.close();
+    }
+
+    public ArrayList<Subject> getAllSubject (Context context){
+
+        DBHelper dbHelper = new DBHelper(context);
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String selectQuery = "SELECT * FROM " + TABLE_SUBJECT;
+        Cursor cursor =database.rawQuery(selectQuery, null);
+
+        ArrayList<Subject> subjectArrayList = new ArrayList<>();
+        if(cursor !=null)
+        {
+            if(cursor.getCount() > 0)
+            {
+                Subject subject = new Subject();
+                while (cursor.moveToNext())
+                {
+
+                    subject.setSubjectName(cursor.getString(1));
+
+                    Log.d("SubjectData", subject.getSubjectName());
+                    subjectArrayList.add(subject);
+                }
+            }
+        }
+        database.close();
+        return subjectArrayList;
     }
 
 }
