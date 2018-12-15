8f729893db775748781c68dd7bc22761fa224f9d
Sonia Rani <>
2018-12-15 14:31:38 -0500

    Committing add note file changes

--- app/src/main/java/com/androidproject/dailynotesandroid/Model/Image.java
+++ app/src/main/java/com/androidproject/dailynotesandroid/Model/Image.java
@@ -4,13 +4,15 @@ public class Image {
 
     private int imageId;
     private String imageLocation;
+    private int noteId;
 
     public Image() {
     }
 
-    public Image(int imageId, String imageLocation) {
+    public Image(int imageId, String imageLocation, int noteId) {
         this.imageId = imageId;
         this.imageLocation = imageLocation;
+        this.noteId = noteId;
     }
 
     public String getImageLocation() {
@@ -28,4 +30,13 @@ public class Image {
     public void setImageId(int imageId) {
         this.imageId = imageId;
     }
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
 }
